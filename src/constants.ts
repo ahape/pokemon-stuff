dea100cb330db83b679a7f6fb60918d8a4462180
Alan Hape <ahape@brightmetrics.com>
2020-12-02 20:54:44 -0800

    Trying to fix stupid VS/TSC caching issue

--- src/constants.ts
+++
@@ -1,5 +0,0 @@
-export const damageRangeMax = 255;
-export const damageRangeMin = 217;
-export const defaultIv = 15; // Assume max possible.
-export const defaultEv = 1; // Assume no effort value.
-export const defaultLevel = 5;
--- src/types/categoryName.ts
+++
@@ -1,4 +0,0 @@
-export type CategoryName =
-    "physical" |
-    "special" |
-    "status";
